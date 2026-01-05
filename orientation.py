d5cd4126b87f9752ce5221b30dbc08143bf2f5fe
Guido Mascia <maskul93@gmail.com>
2026-01-05 15:19:41 -0500

    Added `dcm2eul` function
    Converts a rotation matrix to the Euler angles defined by the given rotation sequence.

--- orientation.py
+++ orientation.py
@@ -72,3 +72,84 @@ def get_linear_acceleration(a_m=None, w_m=None, fs=None, g_global=None, convert_
         a_I /= 9.81
 
     return a_I
+
+'''EULER ANGLES HERE'''
+def dcm2eul(R=None, seq=None):
+    '''
+    Computes the Euler angles from a Rotation Matrix. 
+    '''
+    # These are the 12 allowed sequences of rotations
+    allowed_sequences = ('XYX', 'YXY', 'ZXZ', 'XZX', 'YZY', 'ZYZ', # Proper Euler Angles
+                         'XYZ', 'XZY', 'YXZ', 'YZX', 'ZXY', 'ZYX') # Tait-Bryan Angles (RPY)
+    
+    if seq in allowed_sequences:
+        r11, r12, r13 = R[0]
+        r21, r22, r23 = R[1]
+        r31, r32, r33 = R[2]
+        eul = np.zeros(3)
+
+        if seq == 'XYZ':
+            eul[0] = np.arctan2(-r23, r33)
+            eul[1] = np.arctan2(r13, np.sqrt(1-r13**2))
+            eul[2] = np.arctan2(-r12, r11)
+        
+        if seq == 'XZY':
+            eul[0] = np.arctan2(r32, r22)
+            eul[1] = np.arctan2(-r12, np.sqrt(1-r12**2))
+            eul[2] = np.arctan2(r13, r11)
+
+        if seq == 'XYX':
+            eul[0] = np.arctan2(r21, -r31)
+            eul[1] = np.arctan2(np.sqrt(1-r11**2), r11)
+            eul[2] = np.arctan2(r12, r13)
+
+        if seq == 'XZX':
+            eul[0] = np.arctan2(r31, r21)
+            eul[1] = np.arctan2(np.sqrt(1-r11**2), r11)
+            eul[2] = np.arctan2(r13, -r12)
+
+        if seq == 'YXZ':
+            eul[0] = np.arctan2(r31, r33)
+            eul[1] = np.arctan2(-r23, np.sqrt(1-r23**2))
+            eul[2] = np.arctan2(r21, r22)
+
+        if seq == 'YZX':
+            eul[0] = np.arctan2(-r31, r11)
+            eul[1] = np.arctan2(r21, np.sqrt(1-r21**2))
+            eul[2] = np.arctan2(-r23, r22)
+
+        if seq == 'YXY':
+            eul[0] = np.arctan2(r12, r32)
+            eul[1] = np.arctan2(np.sqrt(1-r22**2), r22)
+            eul[2] = np.arctan2(r21, -r23)
+
+        if seq == 'YZY':
+            eul[0] = np.arctan2(r32, -r12)
+            eul[1] = np.arctan2(np.sqrt(1-r22**2), r22)
+            eul[2] = np.arctan2(r23, r21)
+
+        if seq == 'ZXY':
+            eul[0] = np.arctan2(-r12, r22)
+            eul[1] = np.arctan2(r32, np.sqrt(1-r32**2))
+            eul[2] = np.arctan2(-r31, r33)
+        
+        if seq == 'ZYX':
+            eul[0] = np.arctan2(r21, r11)
+            eul[1] = np.arctan2(-r31, np.sqrt(1-r31**2))
+            eul[2] = np.arctan2(r32, r33)
+            
+        if seq == 'ZXZ':
+            eul[0] = np.arctan2(r13, -r23)
+            eul[1] = np.arctan2(np.sqrt(1-r33**2), r33)
+            eul[2] = np.arctan2(r31, r32)
+
+        if seq == 'ZYZ':
+            eul[0] = np.arctan2(r23, r13)
+            eul[1] = np.arctan2(np.sqrt(1-r33**2), r33)
+            eul[2] = np.arctan2(r32, -r31)
+
+    else:
+        print(f'Wrong sequence. The argument must be one among the following:\n{allowed_sequences}')
+        eul = None
+    
+    return eul
