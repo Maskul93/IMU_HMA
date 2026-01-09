a2810fe5e884d53e62514a265e348fa08164d900
Guido Mascia <maskul93@gmail.com>
2026-01-09 13:55:40 -0500

    Corrected `np.atan2()` implementation.

--- Orientation/Transformations.py
+++ Orientation/Transformations.py
@@ -24,64 +24,64 @@ def dcm2eul(R=None, seq=None):
         eul = np.zeros(3)
 
         if seq == 'XYZ':
-            eul[0] = np.arctan2(r33, -r23)
-            eul[1] = np.arctan2(np.sqrt(1-r13**2), r13)
-            eul[2] = np.arctan2(r11, -r12)
+            eul[0] = np.arctan2(-r23, r33)
+            eul[1] = np.arctan2(r13, np.sqrt(1-r13**2))
+            eul[2] = np.arctan2(-r12, r11)
         
         if seq == 'XZY':
-            eul[0] = np.arctan2(r22, r32)
-            eul[1] = np.arctan2(np.sqrt(1-r12**2), -r12)
-            eul[2] = np.arctan2(r11, r13)
+            eul[0] = np.arctan2(r32, r22)
+            eul[1] = np.arctan2(-r12, np.sqrt(1-r12**2))
+            eul[2] = np.arctan2(r13, r11)
 
         if seq == 'XYX':
-            eul[0] = np.arctan2(-r31 , r21)
-            eul[1] = np.arctan2(r11 , np.sqrt(1-r11**2))
-            eul[2] = np.arctan2(r13 , r12)
+            eul[0] = np.arctan2(r21, -r31)
+            eul[1] = np.arctan2(np.sqrt(1-r11**2), r11)
+            eul[2] = np.arctan2(r12, r13)
 
         if seq == 'XZX':
-            eul[0] = np.arctan2(r21, r31)
-            eul[1] = np.arctan2(r11, np.sqrt(1-r11**2))
-            eul[2] = np.arctan2(-r12, r13)
+            eul[0] = np.arctan2(r31, r21)
+            eul[1] = np.arctan2(np.sqrt(1-r11**2), r11)
+            eul[2] = np.arctan2(r13, -r12)
 
         if seq == 'YXZ':
-            eul[0] = np.arctan2(r33, r31)
-            eul[1] = np.arctan2(np.sqrt(1-r23**2), -r23)
-            eul[2] = np.arctan2(r22, r21)
+            eul[0] = np.arctan2(r31, r33)
+            eul[1] = np.arctan2(-r23, np.sqrt(1-r23**2))
+            eul[2] = np.arctan2(r21, r22)
 
         if seq == 'YZX':
-            eul[0] = np.arctan2(r11, -r31)
-            eul[1] = np.arctan2(np.sqrt(1-r21**2), r21)
-            eul[2] = np.arctan2(r22, -r23)
+            eul[0] = np.arctan2(-r31, r11)
+            eul[1] = np.arctan2(r21, np.sqrt(1-r21**2))
+            eul[2] = np.arctan2(-r23, r22)
 
         if seq == 'YXY':
-            eul[0] = np.arctan2(r32, r12)
-            eul[1] = np.arctan2(r22, np.sqrt(1-r22**2))
-            eul[2] = np.arctan2(-r23, r21)
+            eul[0] = np.arctan2(r12, r32)
+            eul[1] = np.arctan2(np.sqrt(1-r22**2), r22)
+            eul[2] = np.arctan2(r21, -r23)
 
         if seq == 'YZY':
-            eul[0] = np.arctan2(-r12, r32)
-            eul[1] = np.arctan2(r22, np.sqrt(1-r22**2))
-            eul[2] = np.arctan2(r21, r23)
+            eul[0] = np.arctan2(r32, -r12)
+            eul[1] = np.arctan2(np.sqrt(1-r22**2), r22)
+            eul[2] = np.arctan2(r23, r21)
 
         if seq == 'ZXY':
-            eul[0] = np.arctan2(r22, -r12)
-            eul[1] = np.arctan2(np.sqrt(1-r32**2), r32)
-            eul[2] = np.arctan2(r33, -r31)
+            eul[0] = np.arctan2(-r12, r22)
+            eul[1] = np.arctan2(r32, np.sqrt(1-r32**2))
+            eul[2] = np.arctan2(-r31, r33)
 
         if seq == 'ZYX':
-            eul[0] = np.arctan2(r11, r21)
-            eul[1] = np.arctan2(np.sqrt(1-r31**2), -r31)
-            eul[2] = np.arctan2(r33, r32)
+            eul[0] = np.arctan2(r21, r11)
+            eul[1] = np.arctan2(-r31, np.sqrt(1-r31**2))
+            eul[2] = np.arctan2(r32, r33)
    
         if seq == 'ZXZ':
-            eul[0] = np.arctan2(-r23, r13)
-            eul[1] = np.arctan2(r33, np.sqrt(1-r33**2))
-            eul[2] = np.arctan2(r32, r31)
+            eul[0] = np.arctan2(r13, -r23)
+            eul[1] = np.arctan2(np.sqrt(1-r33**2), r33)
+            eul[2] = np.arctan2(r31, r32)
 
         if seq == 'ZYZ':
-            eul[0] = np.arctan2(r13, r23)
-            eul[1] = np.arctan2(r33, np.sqrt(1-r33**2))
-            eul[2] = np.arctan2(-r31, r32)
+            eul[0] = np.arctan2(r23, r13)
+            eul[1] = np.arctan2(np.sqrt(1-r33**2), r33)
+            eul[2] = np.arctan2(r32, -r31)
 
     else:
         print(f'Wrong sequence. The argument must be one among the following:\n{allowed_sequences}')
